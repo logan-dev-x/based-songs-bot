4d212e688f0c7cf86ad6d1b3f6f80008ddec5e8d
Natan <natanscosta7@gmail.com>
2026-05-16 08:41:41 -0300

    recognize date from audio caption

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"regexp"
+	"time"
 
 	tgbot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
+
+func scheduler(bot *tgbot.BotAPI) {
+	for {
+		println("sewrching for schedules")
+
+		msg := tgbot.NewMessage(1071520377, "hi")
+		bot.Send(msg)
+
+		time.Sleep(2 * time.Second)
+	}
+}
 
 func main() {
 	err := godotenv.Load()
@@ -17,7 +31,7 @@ func main() {
 		panic(err)
 	}
 
-	channelId := -1001733966614
+	// channelId := -1001733966614
 
 	bot.Debug = true
 
@@ -27,15 +41,21 @@ func main() {
 
 	updates := bot.GetUpdatesChan(updateConfig)
 
+	// go scheduler(bot)
+
 	for update := range updates {
 		if update.Message.Audio == nil {
 			continue
 		}
 
-		audioMsg := tgbot.NewAudio(int64(channelId), tgbot.FileID(update.Message.Audio.FileID))
-		audioMsg.Caption = "🗿@BasedSongs"
+		// audioMsg := tgbot.NewAudio(int64(channelId), tgbot.FileID(update.Message.Audio.FileID))
+		// audioMsg.Caption = "🗿@BasedSongs"
+		r, _ := regexp.Compile(`([0-9]{2})/([0-9]{2})`)
+		date := r.FindStringSubmatch(update.Message.Caption)
+		content := fmt.Sprintf("dia: %s\nmes: %s", date[1], date[2])
+		msg := tgbot.NewMessage(update.Message.Chat.ID, content)
 
-		if _, err := bot.Send(audioMsg); err != nil {
+		if _, err := bot.Send(msg); err != nil {
 			panic(err)
 		}
 	}
