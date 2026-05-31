7ac2dc4e37f08dee0d1db4f71ac3f59cac40413f
Natan <natanscosta7@gmail.com>
2026-05-30 22:02:29 -0300

    remove unused code

--- internal/song/repository_test.go
+++ internal/song/repository_test.go
@@ -69,26 +69,6 @@ func TestRepository_Save(t *testing.T) {
 			}
 		})
 	}
-	// rows, err := db.Query("SELECT * FROM songs WHERE day = ? AND month = ?;", 2, 5)
-	// if err != nil {
-	// 	t.Errorf("Query returned an unexpected error: %v", err)
-	// }
-	// defer rows.Close()
-	// var result Song
-	// for rows.Next() {
-	// 	if err := rows.Scan(&result.FileID, &result.Day, &result.Month); err != nil {
-	// 		t.Errorf("Scan returned an error: %v", err)
-	// 	}
-	// }
-	// if result.FileID != mockSong.FileID {
-	// 	t.Errorf("Expected %s, but received: %s", mockSong.FileID, result.FileID)
-	// }
-	// if result.Day != mockSong.Day {
-	// 	t.Errorf("Expected %d, but received: %d", mockSong.Day, result.Day)
-	// }
-	// if result.Month != mockSong.Month {
-	// 	t.Errorf("Expected %d, but received: %d", mockSong.Month, result.Month)
-	// }
 }
 
 func TestRepository_GetByDate(t *testing.T) {
