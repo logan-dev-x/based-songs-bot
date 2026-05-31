3e34c96745825ab3689ad036021f5f9ecd5a1eb6
Natan <natanscosta7@gmail.com>
2026-05-30 23:00:25 -0300

    test: return right values when the pattern is right

---
+++ bot/utils_test.go
@@ -0,0 +1,44 @@
+package bot
+
+import "testing"
+
+func Test_extractScheduleDate(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		caption   string
+		wantDay   int
+		wantMonth int
+		wantErr   bool
+	}{
+		{
+			"test with right values",
+			"25/12",
+			25,
+			12,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotDay, gotMonth, gotErr := extractScheduleDate(tt.caption)
+
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("extractScheduleDate() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("extractScheduleDate() succeeded unexpectedly")
+			}
+
+			if gotDay != tt.wantDay {
+				t.Errorf("got day = %v, want %v", gotDay, tt.wantDay)
+			}
+			if gotMonth != tt.wantMonth {
+				t.Errorf("got month = %v, want %v", gotMonth, tt.wantMonth)
+			}
+		})
+	}
+}
