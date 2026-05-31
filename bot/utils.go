c6bcb0de355662ee692771d1469aef9f768dad4a
Natan <natanscosta7@gmail.com>
2026-05-30 23:01:33 -0300

    fix: escape and delimiter and send righ month

--- bot/utils.go
+++ bot/utils.go
@@ -7,14 +7,14 @@ import (
 )
 
 func extractScheduleDate(caption string) (int, int, error) {
-	r, _ := regexp.Compile(`(\d{2})/(\d{2})`)
+	r, _ := regexp.Compile(`(\d{2})\/(\d{2})`)
 	matches := r.FindStringSubmatch(caption)
 	if len(matches) < 3 {
 		return 0, 0, errors.New("não foi possível reconhecer a data do agendamento")
 	}
 
 	day, _ := strconv.Atoi(matches[1])
-	month, _ := strconv.Atoi(matches[1])
+	month, _ := strconv.Atoi(matches[2])
 
 	return day, month, nil
 }
