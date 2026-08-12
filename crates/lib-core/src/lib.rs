b469ecbe8210a3964ac63b884d61f121dd135a01
Natan <natanscosta7@gmail.com>
2026-08-12 14:24:12 -0300

    feat: file path exists

--- crates/lib-core/src/lib.rs
+++ crates/lib-core/src/lib.rs
@@ -1,5 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::path::Path;
+
+fn read_wav<P: AsRef<Path>>(path: P) -> std::io::Result<()> {
+    let _file = std::fs::File::open(path)?;
+    Ok(())
 }
 
 #[cfg(test)]
@@ -7,8 +10,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn file_exists() {
+        let res = read_wav("Cargo.toml");
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn file_not_exists() {
+        let res = read_wav("foo.wav");
+        assert!(res.is_err());
     }
 }
