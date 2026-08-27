68d2f6211ec047302a44b5642a7ad1c5e555f0d3
Natan <natanscosta7@gmail.com>
2026-08-27 13:39:02 -0300

    feat: minimum bot

--- crates/telegram/src/main.rs
+++ crates/telegram/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    println!("Iniciando Based Songs Bot...");
+
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_message(msg.chat.id, "Olá!").await?;
+
+        respond(())
+    })
+    .await;
 }
