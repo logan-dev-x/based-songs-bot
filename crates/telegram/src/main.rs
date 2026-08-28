9bb0aa41777386aaa483edbd4ecb51b7f1809480
Natan <natanscosta7@gmail.com>
2026-08-28 10:58:38 -0300

    feat: download audio file to local machine

--- crates/telegram/src/main.rs
+++ crates/telegram/src/main.rs
@@ -1,4 +1,7 @@
-use teloxide::prelude::*;
+use std::path::PathBuf;
+
+use teloxide::{net::Download, prelude::*};
+use tokio::fs::File;
 
 #[tokio::main]
 async fn main() {
@@ -8,13 +11,18 @@ async fn main() {
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
         if let Some(audio) = msg.audio() {
+            bot.send_message(msg.chat.id, "Baixando áudio").await?;
+            let file = bot.get_file(audio.file.id.clone()).await?;
+            let default_file_name = &String::from("input.mp3");
+            let file_name = audio.file_name.as_ref().unwrap_or(default_file_name);
+            let path = PathBuf::from("/tmp").join(file_name);
+            let mut output = File::create(&path)
+                .await
+                .map_err(|e| teloxide::RequestError::from(std::sync::Arc::new(e)))?;
+            bot.download_file(&file.path, &mut output).await?;
             bot.send_message(
                 msg.chat.id,
-                format!(
-                    "Áudio recebido!\nNome: {}\nTamanho: {} bytes",
-                    audio.file_name.as_deref().unwrap_or("desconhecido"),
-                    audio.file.size,
-                ),
+                format!("Áudio baixado para: {}", path.display()),
             )
             .await?;
         } else {
